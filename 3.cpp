4ee880c780156c563b58ae410ac3ef4691bedf2d
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-28 22:32:03 +0530

    3

---
+++ 3.cpp
@@ -0,0 +1,20 @@
+// to Find the Number of Words in a Given Sentence
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+        char str[50];
+        int count=0,i;
+        cout<<"Enter the string :";
+        gets(str);
+        for( i=0;  str[i]!='\0'; i++){
+            if(str[i] == ' ')
+                count++ ;
+
+        }
+        cout<<"Number of words in the string are:" <<count+1;
+        return 0;
+}
+
+        
+        
