d3b8dc44e424a32323bba99b2ec22a768a014b41
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-28 17:58:32 +0530

    2

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+//to Find the Length of a String
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    char str[20];
+    int len;
+    cout<<"Enter an string:";
+    gets(str);
+    len=strlen(str);
+    cout<<"Length of string is:"<<len;
+    return 0;
+}
