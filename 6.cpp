64f2bae5d4874acb50555f321191877304ce0471
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-29 17:50:23 +0530

    6

--- 6.cpp
+++ 6.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/*#include<iostream>
 #include<string.h>
 using namespace std;
 int main(){
@@ -11,5 +11,22 @@ int main(){
         cin>>str2;
         string concat=str1 +str2 ;
         cout << "Concatenated String : " <<concat;
+        return 0;
+}*/
+//Another method using strcat function:
+//strcat works with C character arrays, 
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+        char str1[20];
+        char str2[20];
+        int i;
+        cout<<"Enter string 1:";
+        cin>>str1;
+        cout<<"Enter string 2:";
+        cin>>str2;
+        strcat(str1, str2);
+        cout<<"Concatenated string :"<<str1;
         return 0;
 }
