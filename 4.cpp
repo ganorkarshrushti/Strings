3634adea7371fb0f2e398e0f480477c77193b65a
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-29 14:28:28 +0530

    4

--- 4.cpp
+++ 4.cpp
@@ -7,7 +7,7 @@ int main() {
     int v = 0, c = 0, n = 0, s = 0;
 
     cout << "Enter a string: ";
-    gets(str);
+    cin>>str;
 
     for (int i = 0; str[i] != '\0'; ++i) {
         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || 
