d6c5478bb61849b0ddec8f91a996df3ff9e884d6
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-30 18:09:47 +0530

    9

--- 9.cpp
+++ 9.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-int main(){
+/*int main(){
          char str[20];
          int i,j,temp;
          cout<<"ENter a string :";
@@ -15,6 +15,24 @@ int main(){
          }
          cout<<"\n Reverse String:"<<str;
          return 0;
-}
+}*/
 
+//Using While loop
+int main(){
+         string str;
+         int i=0,temp;
          
+         cout<<"Enter a string :";
+         cin>>str;
+         int j = str.length() - 1;  // Calculate the length of the entered string  //j is initialized to the last character in the string
+         cout<<"Enter a string :";
+         while(i<j){
+            temp=str[i];
+            str[i]=str[j];
+            str[j]=temp;
+            i++;
+            j--;
+        }
+        cout << "\nReverse string : " << str<<endl;
+        return 0;
+}
